f63974e98ea61e7f71a3a2bfdd1e2b4a91a8d23f
Pedro Silva <pedro-silva.26@outlook.pt>
2025-09-23 17:47:50 +0100

    Add model migrations and app to settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'core',
+    'sensors',
+    'traffic',
+    'vehicles'
 ]
 
 MIDDLEWARE = [
